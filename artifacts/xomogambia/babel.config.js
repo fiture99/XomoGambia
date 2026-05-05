cdc90d436c136a06dc331d6108323970678e71c4
Lamin Jawneh <50606948+fiture99@users.noreply.github.com>
2026-05-05 12:28:39 +0000

    sa

--- artifacts/xomogambia/babel.config.js
+++ artifacts/xomogambia/babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          unstable_transformImportMeta: true,
+        },
+      ],
+    ],
+    plugins: [
+      [
+        "babel-plugin-transform-inline-environment-variables",
+        {
+          include: ["EXPO_ROUTER_APP_ROOT", "EXPO_ROUTER_IMPORT_MODE"],
+        },
+      ],
+    ],
   };
 };
--- artifacts/xomogambia/metro.config.js
+++ artifacts/xomogambia/metro.config.js
@@ -1,3 +1,23 @@
 const { getDefaultConfig } = require("expo/metro-config");
+const path = require("path");
 
-module.exports = getDefaultConfig(__dirname);
+process.env.EXPO_ROUTER_APP_ROOT = "app";
+
+const config = getDefaultConfig(__dirname);
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+config.watchFolders = [workspaceRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+];
+
+config.resolver.extraNodeModules = {
+  "react": path.resolve(projectRoot, "node_modules/react"),
+  "react-native": path.resolve(projectRoot, "node_modules/react-native"),
+};
+
+module.exports = config;
